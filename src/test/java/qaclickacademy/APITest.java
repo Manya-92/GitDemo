e316d8d7566485b1bb8ee91e0453cd924c45f01c
manya <amanya19@gmail.com>
2019-04-25 13:23:10 +0530

    commit develop from India

--- src/test/java/qaclickacademy/APITest.java
+++ src/test/java/qaclickacademy/APITest.java
@@ -6,11 +6,14 @@ public class APITest {
 	@Test
 	public void APItest() {
 		System.out.println("APItest1");
+		System.out.println("APItest1.1");
+		System.out.println("APItest1.2");
 	}
 	
 	@Test
 	public void APItest2() {
 		System.out.println("APItest2");
+		System.out.println("APItest2.1");
 	}
 
 }
