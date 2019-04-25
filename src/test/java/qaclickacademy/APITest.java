558e7b1add2a2c085f28dd37af825efa4b41067d
manya <amanya19@gmail.com>
2019-04-25 11:37:55 +0530

    firstcommit

---
+++ src/test/java/qaclickacademy/APITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test
+	public void APItest() {
+		System.out.println("APItest1");
+	}
+	
+	@Test
+	public void APItest2() {
+		System.out.println("APItest2");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/WebTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class WebTest {
+	
+	@Test
+	public void Webtest() {
+		System.out.println("Webtest1");
+	}
+	
+	@Test
+	public void Webtest2() {
+		System.out.println("Webtest2");
+	}
+
+}
