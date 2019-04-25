33ef44df519057a088c57bdea9b13236698fdf61
manya <amanya19@gmail.com>
2019-04-25 13:38:35 +0530

    commit develop USA

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Appiumtest() {
+		System.out.println("Appiumtest1");
+		System.out.println("Appiumtest1.1");
+		System.out.println("Appiumtest1.2");
+	}
+	
+	@Test
+	public void Appiumtest2() {
+		System.out.println("Appiumtest2");
+		System.out.println("Appiumtest2.1");
+	}
+}
